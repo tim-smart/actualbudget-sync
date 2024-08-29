1cf4fb149a74808216c567969f4a5f3ebe073c49
Tim <hello@timsmart.co>
2024-08-29 15:09:10 +1200

    revert transaction filtering

--- src/Sync.ts
+++ src/Sync.ts
@@ -22,15 +22,10 @@ export const run = (
     yield* Effect.forEach(accounts, ({ bankAccountId, actualAccountId }) =>
       Effect.gen(function* () {
         const transactions = yield* bank.exportAccount(bankAccountId)
-        const ids = transactions.flatMap((t) => (t.id ? [t.id] : []))
-        const imported = yield* actual.findImportedIds(ids)
-        const filtered = transactions.filter(
-          (t) => !t.id || !imported.includes(t.id),
-        )
         yield* actual.use((_) =>
           _.importTransactions(
             actualAccountId,
-            filtered.map((t) => ({
+            transactions.map((t) => ({
               imported_id: t.id,
               date: DateTime.formatIsoDate(t.dateTime),
               payee_name: t.payee,
