566cb7d4e9fc22d25a881d230bae7dcebf66c19d
Tim <hello@timsmart.co>
2024-09-24 16:13:09 +1200

    remove js file

--- src/internal/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configProviderNested = void 0;
-var effect_1 = require("effect");
-var configProviderNested = function (prefix) {
-    return effect_1.ConfigProvider.fromEnv().pipe(effect_1.ConfigProvider.nested(prefix), effect_1.ConfigProvider.constantCase);
-};
-exports.configProviderNested = configProviderNested;
