cb85576d956e424274bde2c15e4e7bb39be6bed8
Tim <hello@timsmart.co>
2024-10-24 11:45:46 +1300

    add test for Akahu

---
+++ .changeset/lemon-ties-join.md
@@ -0,0 +1,5 @@
+---
+"@tim-smart/actualbudget-sync": patch
+---
+
+add test for Akahu
--- src/Sync.ts
+++ src/Sync.ts
@@ -9,7 +9,7 @@ const bigDecimal100 = BigDecimal.fromNumber(100)
 const amountToInt = (amount: BigDecimal.BigDecimal) =>
   amount.pipe(BigDecimal.multiply(bigDecimal100), BigDecimal.unsafeToNumber)
 
-export const run = (options: {
+export const runCollect = (options: {
   readonly accounts: ReadonlyArray<{
     readonly bankAccountId: string
     readonly actualAccountId: string
@@ -19,33 +19,40 @@ export const run = (options: {
     readonly bankCategory: string
     readonly actualCategory: string
   }>
+  readonly categories: ReadonlyArray<{
+    readonly id: string
+    readonly name: string
+  }>
+  readonly payees: ReadonlyArray<{
+    readonly id: string
+    readonly name: string
+    readonly transfer_acct?: string
+  }>
 }) =>
   Effect.gen(function* () {
-    const actual = yield* Actual
     const bank = yield* Bank
     const importId = makeImportId()
 
-    const categories = yield* actual.use((_) => _.getCategories())
     const categoryId = (transaction: AccountTransaction) => {
       const categoryName =
         options.categoryMapping?.find(
           (mapping) => mapping.bankCategory === transaction.category,
         )?.actualCategory ?? transaction.category
-      const category = categories.find(
+      const category = options.categories.find(
         (c) => c.name.toLowerCase() === categoryName?.toLowerCase(),
       )
       return category ? category.id : undefined
     }
 
-    const payees = yield* actual.use((_) => _.getPayees())
     const transferAccountId = (transaction: AccountTransaction) => {
       const transferToAccount = options.accounts.find(
         ({ bankAccountId }) => bankAccountId === transaction.transfer,
       )?.actualAccountId
-      return payees.find((it) => it.transfer_acct === transferToAccount)?.id
+      return options.payees.find((it) => it.transfer_acct === transferToAccount)
+        ?.id
     }
 
-    yield* Effect.forEach(
+    return yield* Effect.forEach(
       options.accounts,
       ({ bankAccountId, actualAccountId }) =>
         Effect.gen(function* () {
@@ -75,15 +82,44 @@ export const run = (options: {
               }
             }),
           )
-          const alreadyImported = yield* actual.findImportedIds(ids)
-          const filtered = forImport.filter(
-            (t) => !alreadyImported.includes(t.imported_id),
-          )
-          yield* actual.use((_) =>
-            _.importTransactions(actualAccountId, filtered),
-          )
+          return {
+            transactions: forImport,
+            ids,
+            actualAccountId,
+          }
         }),
     )
+  })
+
+export const run = (options: {
+  readonly accounts: ReadonlyArray<{
+    readonly bankAccountId: string
+    readonly actualAccountId: string
+  }>
+  readonly categorize: boolean
+  readonly categoryMapping?: ReadonlyArray<{
+    readonly bankCategory: string
+    readonly actualCategory: string
+  }>
+}) =>
+  Effect.gen(function* () {
+    const actual = yield* Actual
+    const categories = yield* actual.use((_) => _.getCategories())
+    const payees = yield* actual.use((_) => _.getPayees())
+
+    const results = yield* runCollect({
+      ...options,
+      categories,
+      payees,
+    })
+
+    for (const { transactions, ids, actualAccountId } of results) {
+      const alreadyImported = yield* actual.findImportedIds(ids)
+      const filtered = transactions.filter(
+        (t) => !alreadyImported.includes(t.imported_id),
+      )
+      yield* actual.use((_) => _.importTransactions(actualAccountId, filtered))
+    }
   })
 
 const makeImportId = () => {
@@ -98,3 +134,51 @@ const makeImportId = () => {
     return `${prefix}-${count}`
   }
 }
+
+export const testCategories = [
+  { id: "1", name: "Transport" },
+  { id: "2", name: "Groceries" },
+  { id: "3", name: "Internet" },
+  { id: "4", name: "Rent" },
+]
+
+export const testPayees = [
+  { id: "1", name: "Acme" },
+  { id: "2", name: "Bobs" },
+  { id: "3", name: "Cafe" },
+  { id: "4", name: "Deli" },
+  { id: "5", name: "Verizon" },
+  { id: "6", name: "Checking", transfer_acct: "actual-checking" },
+  { id: "7", name: "Savings", transfer_acct: "actual-savings" },
+]
+
+export const runTest = (options: {
+  readonly categorize: boolean
+  readonly categoryMapping?: ReadonlyArray<{
+    readonly bankCategory: string
+    readonly actualCategory: string
+  }>
+}) =>
+  Effect.gen(function* () {
+    const results = yield* runCollect({
+      ...options,
+      accounts: [
+        {
+          bankAccountId: "checking",
+          actualAccountId: "actual-checking",
+        },
+        {
+          bankAccountId: "savings",
+          actualAccountId: "actual-savings",
+        },
+      ],
+      categories: testCategories,
+      payees: testPayees,
+    })
+    return results.flatMap((account) =>
+      account.transactions.map((transaction) => ({
+        ...transaction,
+        account: account.actualAccountId,
+      })),
+    )
+  })
